787cdda0b5a53d83e597d296f6d72f136c4684de
alberto <albertoborsatto@hotmail.com>
2025-07-23 22:01:34 -0300

    create product modal style

--- obj/Debug/net9.0/GalpaoEletroLar.AssemblyInfo.cs
+++ obj/Debug/net9.0/GalpaoEletroLar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GalpaoEletroLar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d5af6e668979ab362c7482fdfe104ec19cbf06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33008db021c52a1e1384d4842996eec478a94a6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GalpaoEletroLar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GalpaoEletroLar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
