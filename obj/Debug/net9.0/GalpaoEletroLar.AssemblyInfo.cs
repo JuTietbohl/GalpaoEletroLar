c22620c97e1a6c891740b9d3d8c8da59efd8e316
Alberto Vargas Borsatto <albertoborsatto@hotmail.com>
2025-07-24 16:23:37 -0300

    listing product undone

--- obj/Debug/net9.0/GalpaoEletroLar.AssemblyInfo.cs
+++ obj/Debug/net9.0/GalpaoEletroLar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GalpaoEletroLar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec939812e5063e92f23fea78cfe91be8798bb33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be5e796bd81e7350155f16399ff8f240fce45b08")]
 [assembly: System.Reflection.AssemblyProductAttribute("GalpaoEletroLar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GalpaoEletroLar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/toggleFormInputs.js
+++ wwwroot/js/toggleFormInputs.js
@@ -1,34 +1,38 @@
-﻿const productType = document.getElementById("TipoProduto");
-const productVolts = document.getElementById("campoVoltagem");
-const productSize = document.getElementById("tamanhoProduto");
-const sizeInput = document.getElementById("tamanhoInput");
-const voltsSelect = document.getElementById("voltagemSelect");
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const productType = document.getElementById("TipoProduto");
+    const productVolts = document.getElementById("campoVoltagem");
+    const productSize = document.getElementById("tamanhoProduto");
+    const sizeInput = document.getElementById("tamanhoInput");
+    const voltsSelect = document.getElementById("voltagemSelect");
 
-function updateFields() {
-    const selectedValue = productType.value;
+    function updateFields() {
+        const selectedValue = productType.value;
 
-    if (selectedValue === "Eletro") {
-        productSize.classList.add("d-none");
-        sizeInput.removeAttribute("required");
+        if (selectedValue === "Eletro") {
+            console.log("eletro");
+            productSize.classList.add("d-none");
+            sizeInput.removeAttribute("required");
 
-        productVolts.classList.remove("d-none");
-        voltsSelect.setAttribute("required", "required");
-    } else if (selectedValue === "Movel") {
-        productSize.classList.remove("d-none");
-        sizeInput.setAttribute("required", "required");
+            productVolts.classList.remove("d-none");
+            voltsSelect.setAttribute("required", "required");
+        } else if (selectedValue === "Movel") {
+            console.log("movel");
+            productSize.classList.remove("d-none");
+            sizeInput.setAttribute("required", "required");
 
-        productVolts.classList.add("d-none");
-        voltsSelect.removeAttribute("required");
-    } else {
-        productSize.classList.add("d-none");
-        sizeInput.removeAttribute("required");
+            productVolts.classList.add("d-none");
+            voltsSelect.removeAttribute("required");
+        } else {
+            productSize.classList.add("d-none");
+            sizeInput.removeAttribute("required");
 
-        productVolts.classList.add("d-none");
-        voltsSelect.removeAttribute("required");
+            productVolts.classList.add("d-none");
+            voltsSelect.removeAttribute("required");
+        }
     }
-}
 
-
-productType.addEventListener("change", updateFields);
-
-document.addEventListener("DOMContentLoaded", updateFields);
+    if (productType) {
+        productType.addEventListener("change", updateFields);
+        updateFields();
+    }
+});
